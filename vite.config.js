bcb67d2a2b6b126ded89d3b0f64dcdfe483c7c41
まぷまぷ@DQX <mapmapdqx@gmail.com>
2024-02-25 03:20:55 +0900

    PWA暫定対応

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,37 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA(
+      {
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        },
+        manifest: {
+          name: "トリニティ・タイマー",
+          short_name: "trinity-timer",
+          description: "トリニティ専用タイマー",
+          icons: [
+            {
+              src: "mapmap_dot_Ash1.png",
+              type: "image/png",
+              sizes: "192x192"
+            }
+          ],
+          start_url: "index.html",
+          display: "standalone",
+          background_color: "#ffffff",
+          theme_color: "#000000",
+          lang: "ja"
+        }
+      }
+    )
   ],
   resolve: {
     alias: {
